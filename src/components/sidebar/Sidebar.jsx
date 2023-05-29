b989d3371c445a2810f72138c2679ea303786077
akash-endlos <akash.endlos@gmail.com>
2023-05-29 10:19:48 -0700

    updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from "react";
 import {
   IconButton,
   Avatar,
@@ -21,7 +21,7 @@ import {
   MenuItem,
   MenuList,
   Collapse,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   FiHome,
   FiTrendingUp,
@@ -31,27 +31,29 @@ import {
   FiMenu,
   FiBell,
   FiChevronDown,
-} from 'react-icons/fi';
-import { IconType } from 'react-icons';
-import { ReactText } from 'react';
-import { MdKeyboardArrowRight,MdInventory } from 'react-icons/md'
-import {GiVendingMachine} from 'react-icons/gi'
-import {AiFillCustomerService,AiOutlineUser,AiOutlinePullRequest} from 'react-icons/ai'
-import {GrVmMaintenance} from 'react-icons/gr'
-import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import { logOut, logout, selectCurrentUser } from '@/redux/feature/authSlice';
-import { useRouter } from 'next/router';
+} from "react-icons/fi";
+import { IconType } from "react-icons";
+import { ReactText } from "react";
+import { MdKeyboardArrowRight, MdInventory } from "react-icons/md";
+import { GiVendingMachine } from "react-icons/gi";
+import {
+  AiFillCustomerService,
+  AiOutlineUser,
+  AiOutlinePullRequest,
+} from "react-icons/ai";
+import { GrVmMaintenance } from "react-icons/gr";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut, logout, selectCurrentUser } from "@/redux/feature/authSlice";
+import { useRouter } from "next/router";
 
-export default function SidebarWithHeader({
-  children
-}) {
+export default function SidebarWithHeader({ children }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <SidebarContent
         onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
+        display={{ base: "none", md: "block" }}
       />
       <Drawer
         autoFocus={false}
@@ -60,24 +62,21 @@ export default function SidebarWithHeader({
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        size="full">
+        size="full"
+      >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
       </Drawer>
       {/* mobilenav */}
       <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} >
-        {children}
-      </Box>
+      <Box ml={{ base: 0, md: 60 }}>{children}</Box>
     </Box>
   );
 }
 
-
-
 const SidebarContent = ({ onClose, ...rest }) => {
-  const [MenuLinks, setMenuLinks] = useState([])
+  const [MenuLinks, setMenuLinks] = useState([]);
 
   const [integrations, setIntegrations] = useState({});
 
@@ -89,67 +88,84 @@ const SidebarContent = ({ onClose, ...rest }) => {
   };
   const navItems = [
     {
-      title: 'Dashboard', link: 'dashboard', icon: FiHome
+      title: "Dashboard",
+      link: "/dashboard",
+      icon: FiHome,
     },
     {
-      title: 'Customer',icon:AiFillCustomerService,link:"customer",
+      title: "Customer",
+      icon: AiFillCustomerService,
+      link: "/customer",
       // subItems: [
       //   { title: 'Customer 1' },
       //   { title: 'Customer 2' },
       // ],
     },
     {
-      title: 'Inventory',icon:MdInventory,link:"inventory",
-
+      title: "Inventory",
+      icon: MdInventory,
+      link: "",
+      subItems: [
+        { title: "Inventory Type", link: "/inventory/inventory-type" },
+        { title: "inventory Details", link: "/inventory/inventory-details" },
+      ],
     },
     {
-      title: 'Machine Enrolment',icon:GiVendingMachine,link:"machine-enrolment",
+      title: "Machine Enrolment",
+      icon: GiVendingMachine,
+      link: "/machine-enrolment",
       // subItems: [
       //   { title: 'Machine ID',link: '/machine-enrolment/machine-id' },
       //   { title: 'Machine Enrollment',link:"/machine-enrolment", },
       // ],
     },
     {
-      title: 'User Management',icon:AiOutlineUser,
+      title: "User Management",
+      icon: AiOutlineUser,
       subItems: [
-        { title: 'Add Role',link: '' },
-        { title: 'Add User',link:"" },
+        { title: "Add Role", link: "" },
+        { title: "Add User", link: "" },
       ],
     },
     {
-      title: 'Maintenance',icon:GrVmMaintenance,link:"",
+      title: "Maintenance",
+      icon: GrVmMaintenance,
+      link: "",
       subItems: [
-        { title: 'Add Parts' ,link:"",},
-        { title: 'Add Problems',link:"", },
-        { title: 'Add Solutions',link:"", },
+        { title: "Add Parts", link: "" },
+        { title: "Add Problems", link: "" },
+        { title: "Add Solutions", link: "" },
       ],
     },
     {
-      title: 'Complaints',icon:AiOutlinePullRequest,link:"",
+      title: "Complaints",
+      icon: AiOutlinePullRequest,
+      link: "",
     },
   ];
 
-  const [user, setuser] = useState('superadmin')
+  const [user, setuser] = useState("superadmin");
   useEffect(() => {
-    if (user === 'superadmin') {
-      setMenuLinks(navItems)
+    if (user === "superadmin") {
+      setMenuLinks(navItems);
     }
-  }, [user])
+  }, [user]);
   return (
     <Box
       transition="3s ease"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
       pos="fixed"
       h="full"
-      {...rest}>
+      {...rest}
+    >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
           Endlos RVM
         </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       <Flex
         direction="column"
@@ -161,15 +177,19 @@ const SidebarContent = ({ onClose, ...rest }) => {
         <div>
           {navItems.map((item, index) => (
             <React.Fragment key={index}>
-               {/* <Link href={item.link}>
+              {/* <Link href={item.link}>
                <NavItem onClick={() => toggleItem(index)} link={item.link} icon={item.icon}>{item.title}</NavItem>
                </Link> */}
               {item.link ? (
                 <Link href={item.link}>
-                  <NavItem onClick={() => toggleItem(index)} icon={item.icon}>{item.title}</NavItem>
+                  <NavItem onClick={() => toggleItem(index)} icon={item.icon}>
+                    {item.title}
+                  </NavItem>
                 </Link>
               ) : (
-                <NavItem onClick={() => toggleItem(index)} icon={item.icon}>{item.title}</NavItem>
+                <NavItem onClick={() => toggleItem(index)} icon={item.icon}>
+                  {item.title}
+                </NavItem>
               )}
               {item.subItems && (
                 <Collapse in={integrations[index]}>
@@ -190,57 +210,58 @@ const SidebarContent = ({ onClose, ...rest }) => {
   );
 };
 
-const NavItem = ({ icon,link, children, ...rest }) => {
+const NavItem = ({ icon, link, children, ...rest }) => {
   return (
-    
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: 'cyan.400',
-          color: 'white',
-        }}
-        {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: 'white',
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
+    <Flex
+      align="center"
+      p="4"
+      mx="4"
+      borderRadius="lg"
+      role="group"
+      cursor="pointer"
+      _hover={{
+        bg: "cyan.400",
+        color: "white",
+      }}
+      {...rest}
+    >
+      {icon && (
+        <Icon
+          mr="4"
+          fontSize="16"
+          _groupHover={{
+            color: "white",
+          }}
+          as={icon}
+        />
+      )}
+      {children}
+    </Flex>
   );
 };
 
-const MobileNav = ({ onOpen, ...rest}) => {
-  const dispatch = useDispatch()
-  const user = useSelector(selectCurrentUser)
-  const router = useRouter()
-  const handleLogout=()=>{
-    dispatch(logout({user:null,token:null}))
-    router.push('/')
-  }
+const MobileNav = ({ onOpen, ...rest }) => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectCurrentUser);
+  const router = useRouter();
+  const handleLogout = () => {
+    dispatch(logout({ user: null, token: null }));
+    router.push("/");
+  };
   return (
     <Flex
       ml={{ base: 0, md: 60 }}
       px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent={{ base: 'space-between', md: 'flex-end' }}
-      {...rest}>
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}
+    >
       <IconButton
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: "flex", md: "none" }}
         onClick={onOpen}
         variant="outline"
         aria-label="open menu"
@@ -248,40 +269,45 @@ const MobileNav = ({ onOpen, ...rest}) => {
       />
 
       <Text
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: "flex", md: "none" }}
         fontSize="2xl"
         fontFamily="monospace"
-        fontWeight="bold">
+        fontWeight="bold"
+      >
         Endlos RVM
       </Text>
 
-      <HStack spacing={{ base: '0', md: '6' }}>
+      <HStack spacing={{ base: "0", md: "6" }}>
         {/* <IconButton
           size="lg"
           variant="ghost"
           aria-label="open menu"
           icon={<FiBell />}
         /> */}
-        <Flex alignItems={'center'}>
+        <Flex alignItems={"center"}>
           <Menu>
             <MenuButton
               py={2}
               transition="all 0.3s"
-              _focus={{ boxShadow: 'none' }}>
+              _focus={{ boxShadow: "none" }}
+            >
               <HStack>
                 <Avatar
-                  className='w-10'
-                  size={'sm'}
+                  className="w-10"
+                  size={"sm"}
                   src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
                   }
                 />
                 <VStack
-                  display={{ base: 'none', md: 'flex' }}
+                  display={{ base: "none", md: "flex" }}
                   alignItems="flex-start"
                   spacing="1px"
-                  ml="2">
-                  <Text fontSize="sm">{user?.firstname} {user?.lastname}</Text>
+                  ml="2"
+                >
+                  <Text fontSize="sm">
+                    {user?.firstname} {user?.lastname}
+                  </Text>
                   <Text fontSize="xs" color="gray.600">
                     {user?.role}
                   </Text>
@@ -292,10 +318,18 @@ const MobileNav = ({ onOpen, ...rest}) => {
               </HStack>
             </MenuButton>
             <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem className='py-2 px-12 rounded-sm border'>Profile</MenuItem>
-              <MenuItem className='py-2 px-12 rounded-sm border' onClick={()=>handleLogout()}>Sign out</MenuItem>
+              bg={useColorModeValue("white", "gray.900")}
+              borderColor={useColorModeValue("gray.200", "gray.700")}
+            >
+              <MenuItem className="py-2 px-12 rounded-sm border">
+                Profile
+              </MenuItem>
+              <MenuItem
+                className="py-2 px-12 rounded-sm border"
+                onClick={() => handleLogout()}
+              >
+                Sign out
+              </MenuItem>
             </MenuList>
           </Menu>
         </Flex>
