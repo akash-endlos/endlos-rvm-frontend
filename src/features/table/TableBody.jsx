334b2fc1cdc6d9f57dc9eefd0513c93cf3df470e
akash-endlos <akash.endlos@gmail.com>
2023-05-29 10:10:02 -0700

    updated

--- src/features/table/TableBody.jsx
+++ src/features/table/TableBody.jsx
@@ -3,12 +3,12 @@ import { Tbody, Td, Tr } from "@chakra-ui/react";
 const TableBody = ({ headerNames, paginatedData, renderAction }) => {
 
   const showBranches = (items) => {
-    if (items.length === 0) {
+    if (items?.length === 0) {
       return <div>No branches available</div>;
     }
     let renderedItems = [];
 
-    if (items.length > 0) {
+    if (items?.length > 0) {
       renderedItems = items.slice(0, 2).map((item, index) => (
         <span key={index}>{item.name} / </span>
       ));
