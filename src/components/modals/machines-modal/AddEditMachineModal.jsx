e6ca738a9d7d7b508f2a14a753cb3c1537bf134e
akash-endlos <akash.endlos@gmail.com>
2023-06-08 12:21:20 +0530

    update machine

--- src/components/modals/machines-modal/AddEditMachineModal.jsx
+++ src/components/modals/machines-modal/AddEditMachineModal.jsx
@@ -12,14 +12,16 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Select,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-const AddEditMachineModal = ({ isOpen, onClose, onSave, rowData,onEditSave }) => {
+const AddEditMachineModal = ({ isOpen, onClose, onSave, rowData, onEditSave }) => {
   const isEditMode = !!rowData;
   const [formData, setFormData] = useState({});
+  const [dropdowns, setDropdowns] = useState([{ category: "", subcategory: "" }]);
 
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
@@ -53,19 +55,39 @@ const AddEditMachineModal = ({ isOpen, onClose, onSave, rowData,onEditSave }) =>
     }
   }, [isOpen, isEditMode, rowData, reset, setValue]);
 
+  const categoryOptions = ["Category 1", "Category 2", "Category 3"];
+  const subcategoryOptions = {
+    "Category 1": ["Subcategory 1.1", "Subcategory 1.2"],
+    "Category 2": ["Subcategory 2.1", "Subcategory 2.2"],
+    "Category 3": ["Subcategory 3.1", "Subcategory 3.2"],
+  };
+
+  const handleDropdownChange = (index, field, value) => {
+    const updatedDropdowns = [...dropdowns];
+    updatedDropdowns[index][field] = value;
+    setDropdowns(updatedDropdowns);
+  };
+
+  const addDropdown = () => {
+    setDropdowns([...dropdowns, { category: "", subcategory: "" }]);
+  };
+
+  const removeDropdown = (index) => {
+    const updatedDropdowns = [...dropdowns];
+    updatedDropdowns.splice(index, 1);
+    setDropdowns(updatedDropdowns);
+  };
+
   const onSubmit = (data) => {
-    if(isEditMode)
-    {
-      onEditSave(data)
+    if (isEditMode) {
+      onEditSave(data);
+      onClose();
+      reset();
+    } else {
+      onSave(data);
       onClose();
       reset();
     }
-    else{
-      onSave(data);
-      onClose();
-      reset(); 
-    }
-   // Reset the form values
   };
 
   return (
@@ -83,6 +105,56 @@ const AddEditMachineModal = ({ isOpen, onClose, onSave, rowData,onEditSave }) =>
                 {errors.name && errors.name.message}
               </FormErrorMessage>
             </FormControl>
+
+            {dropdowns.map((dropdown, index) => (
+              <div key={index}>
+                <FormControl mt={4}>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    value={dropdown.category}
+                    onChange={(e) =>
+                      handleDropdownChange(index, "category", e.target.value)
+                    }
+                  >
+                    <option value="">Select Category</option>
+                    {categoryOptions.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+
+                <FormControl mt={4}>
+                  <FormLabel>Subcategory</FormLabel>
+                  <Select
+                    value={dropdown.subcategory}
+                    onChange={(e) =>
+                      handleDropdownChange(index, "subcategory", e.target.value)
+                    }
+                  >
+                    <option value="">Select Subcategory</option>
+                    {subcategoryOptions[dropdown.category]?.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+
+                <Button
+                  variant="link"
+                  colorScheme="red"
+                  onClick={() => removeDropdown(index)}
+                >
+                  Remove
+                </Button>
+              </div>
+            ))}
+
+            <Button variant="link" colorScheme="green" mt={4} onClick={addDropdown}>
+              Add Dropdown
+            </Button>
           </ModalBody>
           <ModalFooter>
             <Button variant="ghost" onClick={onClose}>
